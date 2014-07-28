ced4357e5a4af71c22d5c4a75acf4be140328ab3
ozanasan <ozanasan@gmail.com>
2014-07-28 13:43:46 +0300

    003 : Adding the dispatch for main thread reload

--- NerdFeed/BNRCoursesViewController.m
+++ NerdFeed/BNRCoursesViewController.m
@@ -11,10 +11,18 @@
 @interface BNRCoursesViewController()
 
 @property (nonatomic) NSURLSession *session;
+@property (nonatomic, copy) NSArray *courses;
 
 @end
 
 @implementation BNRCoursesViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+}
 
 - (instancetype)initWithStyle:(UITableViewStyle)style
 {
@@ -33,12 +41,17 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [self.courses count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    
+    NSDictionary *course = self.courses[indexPath.row];
+    cell.textLabel.text = course[@"title"];
+    
+    return cell;
 }
 
 - (void)fetchFeed
@@ -51,7 +64,14 @@
                                                  completionHandler:
                                       ^(NSData *data, NSURLResponse *response, NSError *error){
                                           NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-                                          NSLog(@"%@", jsonObject);
+                                          self.courses = jsonObject[@"courses"];
+                                          
+                                          NSLog(@"%@", self.courses);
+                                          
+                                          dispatch_async(dispatch_get_main_queue(), ^(void)
+                                          {
+                                              [self.tableView reloadData];
+                                          });
                                       }];
     [dataTask resume];
 }
